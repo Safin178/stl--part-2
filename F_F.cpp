a4275c8a498334eb498417d96d37ab121c24b199
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-10-24 08:25:57 +0600

    h

--- F_F.cpp
+++ F_F.cpp
@@ -1,70 +1,152 @@
-//----------- Bismillahir Rahmanir Raheem -------------/
+// Bismillahir Rahmanir Raheem
 #include<bits/stdc++.h>
 using namespace std;
 
-typedef vector<long long> vl;
+//typedef data type define korae
 typedef vector<int> vi;
+typedef long long ll;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
 
-#define all(v)	   ((v).begin()), ((v).end())
-#define sz(v)	   ((int)((v).size()))
-#define ll         long long
-#define pb         push_back
-#define nl         '\n'
-#define yes        cout<<"YES\n"
-#define no         cout<<"NO\n"
-#define ryes       {cout<<"YES\n";return;}
-#define rno        {cout<<"NO\n";return;}
-#define vit        vector<int>::iterator  
-#define forcin(n)  for(auto &x : n) cin>>x;
-#define forcout(n) for(auto x : n) cout << x << " ";
-#define set_sail    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define ever       ;1;
-#define ff         first
-#define ss         second
+#define bismillah ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(a) (a).begin(),(a).end()
+#define pb push_back 
+#define forcin(n) for(auto &x : n) cin >> x
+#define forcout(n) for(auto x : n) cout << x << " "
+#define sz(n) (int)n.size()
+#define nl '\n'
+#define YES cout<< "YES\n"
+#define NO cout<< "NO\n"
+#define F first
+#define S second
+#define mem(a,b) memset(a,b,sizeof(a))      // 0 n -1 only
+#define gcd(a,b) __gcd(a,b)
+#define set_pre(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
 #define mp make_pair
-void print(auto v2, string s = "")
-{
-	cout<<s;
-	for (int i = 0; i < (int)v2.size(); ++i)
-			cout<< v2[i] <<" ";
-		cout<<"\n";
-}
-void printpv(auto v2, string s = "")
-{
-	cout<<s;
-	for (int i = 0; i < (int)v2.size(); ++i)
-			cout<< v2[i].ff <<" " <<v2[i].ss << nl;
-		cout<<"\n";
+#define ever       ;1;
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+
+void printVec(vector<int> v , string s=""){
+    cout << s <<' ';
+    for(int i=0;i<(int)v.size();i++)
+        cout << v[i] << " ";
+    cout << "\n";
 }
 
-/*        ／＞　　フ
-　　　　　| 　_　 _ |
-　 　　　／` ミ＿Yノ     meow
-　　 　 /　　　 　 |   
-　　　 /　 ヽ　　 ﾉ  
-　 　 │　　|　|　|
-　／￣|　　 |　|　|    
-　| (￣ヽ＿_ヽ_)__)   
-　＼二つ
-*/
+bool is_vowel(char ch){
+    string s="aeiouAEIOU";
+    for(int i=0;s[i];i++){
+        if(s[i]==ch){
+            return 1;
+        }
+    }
+    return 0;
+}
 
-void solve()
-{
-      
+//Debugger
+ //pair
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+            return os << "(" << p.first << ", " << p.second << ")";
+}
+//vector
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+            os << "{";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "}";
+}
+//set
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "]";
+}
+//multiset
+template < typename T >
+ostream &operator << ( ostream & os, const multiset< T > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "]";
+}
+//map
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << it -> first << " = " << it -> second ;
+                                            }
+                    return os << "]";
+}
 
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+ 
+void faltu () {
+            cerr << endl;
+}
+//array
+template <typename T>
+void faltu( T a[], int n ) {
+            for(int i = 0; i < n; ++i) cerr << a[i] << ' ';
+                cerr << endl;
+}
+
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest) {
+            cerr << arg << ' ';
+                faltu(rest...);
+}
+ 
+
+/*____________________________________________________________________________________________________________________________________*/
+
+
+
+void solve(){
+    string s;
+    cin >> s;
+    stack<char>k;
+    for(int i = 0; i < sz(s); i++){
+        
+        if(k.empty())k.push(s[i]);
+        else if( (k.top() == '(' && s[i] == ')') || (k.top() == '{' && s[i] == '}') || (k.top() == '[' && s[i] == ']') )
+        {
+            //cout << k.top() << " ";
+            k.pop();
+        }
+        else k.push( s[i] );
+    }
+   // cout << sz(k) <<  " ";
+    if(sz(k)>0)NO;
+    else YES;
+
+    
 
 }
 
 int main(){
-     set_sail;
+    
+    bismillah
     int t=1;
-   // cin >> t;          // remove '//' for testcase
+    cin >> t;          // remove '//' for testcase
     while(t--){
         solve();
     }
     return 0;
 }
-
-
-
-
